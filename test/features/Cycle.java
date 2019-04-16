a5a39f4e64ae276d05abca641616438f9eb6a1c1
Ben Donaldson <Ben2217@sky.com>
2019-04-16 12:50:37 +0100

    added second test

--- test/features/Cycle.java
+++ test/features/Cycle.java
@@ -2,17 +2,33 @@ package features;
 
 import static org.junit.Assert.*;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class Cycle {
 	
+	Service service = new Service();
 	
+	@BeforeClass
+	public void setup() {
+		service.addAccount("Ben", "Donaldson", "1");
+		service.addAccount("Be", "Donaldson", "2");
+		service.addAccount("B", "Donaldson", "3");
+		service.addAccount("Be", "Donaldson", "4");
+		service.addAccount("Ben", "Donaldson", "5");
+		
+	}
 	
 	@Test
-	public void count() {
-		Service service = new Service();
+	public void Test1() {
 		assertEquals(0, service.count(""));
 	}
+	
+	public void Test2() {
+		assertEquals(1,service.count("B"));
+	}
+	
+	
 	
 
 }
